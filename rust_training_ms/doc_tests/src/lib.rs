914691d273ac459fb8fc8bb68732cbbc1e3139bb
barrett <onkoe@protonmail.com>
2023-01-12 13:29:29 -0600

    added doc tests project exercise

---
+++ rust_training_ms/doc_tests/src/lib.rs
@@ -0,0 +1,51 @@
+/// This function divides two numbers.
+///
+/// # Example #1: 10 / 2 == 5
+///
+/// ```
+/// let result = doc_tests::div(10, 2); 
+/// assert_eq!(result, 5);
+/// ```
+///
+/// # Example #2: 6 / 3 = 2
+///
+/// ```
+/// let result = doc_tests::div(6, 3);
+/// assert_eq!(result, 2);
+/// ```
+///
+/// # Panics
+///
+/// The function panics if the second argument is zero.
+///
+/// ```rust,should_panic
+/// // panics on division by zero
+/// #[should_panic]
+/// let result = doc_tests::div(32, 0);
+/// assert_eq!(result, 5);
+/// ```
+pub fn div(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    }
+    a / b
+}
+
+/// This function subtracts two numbers.
+///
+/// # Example #1: 9 - 2 == 7
+///
+/// ```
+/// let result = doc_tests::sub(9, 2);
+/// assert_eq!(result, 7);
+/// ```
+///
+/// # Example #2: 6 - 9 == -3
+///
+/// ```
+/// let result = doc_tests::sub(6, 9);
+/// assert_eq!(result, -3);
+/// ```
+pub fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
