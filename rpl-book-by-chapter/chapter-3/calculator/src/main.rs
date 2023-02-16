4ddf844e7b4b754de1a41956a7fda5810e778e35
barrett <onkoe@protonmail.com>
2023-02-15 20:08:39 -0600

    Finished Calculator (ch3) project!

--- rpl-book-by-chapter/chapter-3/calculator/src/main.rs
+++ rpl-book-by-chapter/chapter-3/calculator/src/main.rs
@@ -1,12 +1,12 @@
-use std::{io, str::FromStr};
+use std::{io, io::Write, str::FromStr};
 
 fn main() {
     let mut inputs: (Option<f64>, Option<f64>, Option<char>) = (None, None, None);
 
     while inputs.0 == None {
         print!("Please provide your first number: ");
+        io::stdout().flush().expect("flush failed.");
         let given_user_string: Option<String> = get_user_input();
-        println!();
 
         inputs.0 = match given_user_string {
             Some(string) => match f64::from_str(&string.trim()) {
@@ -15,14 +15,12 @@ fn main() {
             },
             None => None,
         };
-
-        println!("{:#?}", inputs);
     }
 
     while inputs.1 == None {
         print!("Please provide your second number: ");
+        io::stdout().flush().expect("flush failed.");
         let given_user_string: Option<String> = get_user_input();
-        println!();
 
         inputs.1 = match given_user_string {
             Some(string) => match f64::from_str(&string.trim()) {
@@ -31,36 +29,40 @@ fn main() {
             },
             None => None,
         };
-
-        println!("{:#?}", inputs);
     }
 
     while inputs.2 == None {
         print!("Please provide your operation [+, -, x, /]: ");
+        io::stdout().flush().expect("flush failed.");
         let given_user_string: Option<String> = get_user_input();
-        println!();
 
         inputs.2 = match given_user_string {
             Some(string) => match char::from_str(&string.trim()) {
                 Ok(operation) => match operation {
-                    '+' | '-' | 'x' | '/' => Some(operation),
+                    '+' | '-' | 'x' | '/' | '*' => Some(operation),
                     _ => None,
                 },
                 Err(_) => None,
             },
             None => None,
         };
-
-        println!("{:#?}", inputs);
     }
 
+    let calculator_result = match calculate(
+        unwrap_option(inputs.0.clone()),
+        unwrap_option(inputs.1.clone()),
+        unwrap_option(inputs.2.clone()),
+    ) {
+        Some(data) => data,
+        None => panic!("Invalid data receieved by calculator!"),
+    };
+
     println!(
-        "{:?}",
-        calculate(
-            unwrap_option(inputs.0),
-            unwrap_option(inputs.1),
-            unwrap_option(inputs.2),
-        )
+        "{} {} {} = {}",
+        unwrap_option(inputs.0.clone()),
+        unwrap_option(inputs.2.clone()),
+        unwrap_option(inputs.1.clone()),
+        calculator_result
     );
 }
 
@@ -69,11 +71,11 @@ fn get_user_input() -> Option<String> {
 
     match io::stdin().read_line(&mut user_input_string) {
         Ok(_) => {
-            println!("user_input_string: {}", user_input_string);
+            //println!("user_input_string: {}", user_input_string); TODO: asdf
             return Some(user_input_string);
         }
         Err(_) => {
-            println!("user_input_string: {}", user_input_string);
+            //println!("user_input_string: {}", user_input_string); TODO: asdf
             return None;
         }
     }
@@ -90,7 +92,7 @@ fn calculate(number_1: f64, number_2: f64, operation: char) -> Option<f64> {
     let result: Option<f64>;
 
     match operation {
-        'x' => result = Some(number_1 * number_2),
+        'x' | '*' => result = Some(number_1 * number_2),
         '/' => result = Some(number_1 / number_2),
         '+' => result = Some(number_1 + number_2),
         '-' => result = Some(number_1 - number_2),
